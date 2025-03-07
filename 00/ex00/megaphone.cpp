c6ece2c69595aec085e655e1413bda297c06aef1
aindjare <aindjare@c4r3p1.1337.ma>
2025-03-07 15:11:30 +0000

    fix(00): ex00 no longer edits the argv, argc in main

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,23 +6,24 @@
 /*   By: aindjare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 18:58:19 by aindjare          #+#    #+#             */
-/*   Updated: 2024/12/21 19:13:24 by aindjare         ###   ########.fr       */
+/*   Updated: 2025/03/07 15:04:58 by aindjare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include <string>
+#include <iostream>
 
 int	main(int argc, const char **argv) {
 	if (argc < 2) {
-		argv[1] = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-		argc++;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
 	}
 	for (int i = 1; i < argc; i++) {
-		std::string	str = argv[i];
-		for (size_t j = 0; j < str.length(); j++)
-			str[j] = std::toupper(str[j]);
-		std::cout << str;
+		std::string	arg = argv[i];
+		for (size_t j = 0; j < arg.length(); j++)
+			arg[j] = std::toupper(arg[j]);
+
+		std::cout << arg;
 		if (i == argc - 1)
 			std::cout << std::endl;
 	}
