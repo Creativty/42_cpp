43be2f65380ad8577940677a03d3bd67aa374b8f
aindjare <aindjare@c4r3p1.1337.ma>
2025-03-12 14:07:01 +0000

    fix(01): ex02 is done.

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aindjare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 13:26:20 by aindjare          #+#    #+#             */
-/*   Updated: 2024/12/23 13:29:56 by aindjare         ###   ########.fr       */
+/*   Updated: 2025/03/12 14:06:10 by aindjare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 #include <iostream>
 
 int	main(void) {
-	std::string		str("HI THIS IS BRAIN");
-	std::string		*str_ptr = &str;
+	std::string		str = "HI THIS IS BRAIN";
+	std::string*	str_ptr = &str;
 	std::string&	str_ref(str);
 
 	std::cout << "Memory address of str    : " << &str << std::endl;
-	std::cout << "Memory address of str_ptr: " << &str_ptr << std::endl;
-	std::cout << "Memory address of str_ref: " << &str_ref << std::endl;
+	std::cout << "Memory address in str_ptr: " <<  str_ptr << std::endl;
+	std::cout << "Memory address in str_ref: " << &str_ref << std::endl;
 
-	std::cout << "----" << std::endl;
+	std::cout << std::endl;
 
 	std::cout << "Value of str    : " <<  str << std::endl;
 	std::cout << "Value of str_ptr: " << *str_ptr << std::endl;
